4d47e5ea3431ca5610ac6e93c7020e5ae03701f1
daleydeng <daleydeng@gmail.com>
2024-12-08 10:32:06 +0800

    update recipe.yaml

--- conda_recipe/build.sh
+++ conda_recipe/build.sh
@@ -1,3 +1,7 @@
 mkdir -p $PREFIX/{include/camport3,lib}
 cp -ar include/*.h $PREFIX/include/camport3
-cp -ar lib/linux/lib_x64/* $PREFIX/lib
+if [ "$ARCH" = "64" ]; then
+    cp -ar lib/linux/lib_x64/* $PREFIX/lib
+elif [ "$ARCH" = "aarch64" ]; then
+    cp -ar lib/linux/lib_Aarch64/* $PREFIX/lib
+fi
